ccac982b75ea84b77c5e63ca1304667e415faa98
Giubis <116124169+Giubis@users.noreply.github.com>
2025-07-07 17:37:54 +0100

    Add some basic style to the Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <>
+    <header>
       <h1>WIZR | Knowledge is Power</h1>
       <Navbar />
-    </>
+    </header>
   );
 }
 
