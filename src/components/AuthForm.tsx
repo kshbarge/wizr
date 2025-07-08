959b1c757533046b40209d18b2674c9d27dd073f
Giubis <116124169+Giubis@users.noreply.github.com>
2025-07-08 18:20:55 +0100

    Add error and success popup alerts, implement handlePasswordClick to briefly check password authentication

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -12,6 +12,8 @@ const AuthForm: FC = () => {
 
   const isValidEmail = emailRegex.test(email);
 
+  const [password, setPassword] = useState<string>("");
+
   const handleEmailClick = (): void => {
     Swal.fire({
       position: "top-end",
@@ -63,6 +65,53 @@ const AuthForm: FC = () => {
     }, 1500);
   };
 
+  const handlePasswordClick = (): void => {
+    Swal.fire({
+      position: "top-end",
+      title: "Verifying password...",
+      customClass: {
+        popup: "swal-popup",
+        title: "swal-title",
+        loader: "swal-loading",
+      },
+      didOpen: () => {
+        Swal.showLoading();
+      },
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+    });
+
+    setTimeout(() => {
+      if (password === API.password) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          iconColor: "#fdd673",
+          title: "Access granted!",
+          text: "You have successfully logged in.",
+          customClass: {
+            popup: "swal-popup swal-popup--success",
+            title: "swal-title",
+            confirmButton: "swal-button",
+          },
+        });
+      } else {
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          iconColor: "#fdd673",
+          title: "Incorrect password",
+          text: "Please, try again.",
+          customClass: {
+            popup: "swal-popup swal-popup--error",
+            title: "swal-title",
+            confirmButton: "swal-button",
+          },
+        });
+      }
+    }, 1500);
+  };
+
   return (
     <form className="auth-form">
       <label htmlFor="email">Email:</label>
@@ -72,17 +121,28 @@ const AuthForm: FC = () => {
         value={email}
         placeholder="Enter your email to login or sign up"
         onChange={(event) => setEmail(event.target.value)}
+        disabled={isRegistered}
         required
       />
-      <button type="button" onClick={handleEmailClick} disabled={!isValidEmail}>
+      <button
+        type="button"
+        onClick={handleEmailClick}
+        disabled={!isValidEmail || isRegistered}
+      >
         Next
       </button>
 
       {isRegistered && (
         <div className="password-field">
           <label htmlFor="password">Password:</label>
-          <input id="password" type="password" required />
-          <button type="button" onClick={() => console.log("registered")}>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <button type="button" onClick={handlePasswordClick}>
             Login
           </button>
         </div>
