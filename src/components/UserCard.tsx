7a1e1410cc67b33de10ef17f5bb4229d95c1e7da
Shannel Feranand <shannelferanand@Shannels-MacBook-Pro.local>
2025-07-09 17:24:18 +0100

    feature:2.3 add dropdown selector for skills with custom styling

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 interface UserCardProps {
     username: string;
     avatar: string;
@@ -21,7 +22,7 @@ function UserCard({
             onSave(username, avatar);
         }
     }
-    const handleImageChange = (event: React.ChangeEvent<HTMLFormElement>) => {
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file){
             const imageURL= URL.createObjectURL(file);
@@ -29,33 +30,54 @@ function UserCard({
         }
     }
     
-    return (
-        <form onSubmit={handleSubmit}>
+    return ( 
+       <div className="card">
+        <div className="imgBx">
+        {avatar ? (
+            <img 
+              src={avatar} 
+              alt="avatar"/>
+        ) :( 
+            <img src="https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=mail@ashallendesign.co.uk"
+             alt="default avatar"/>
+            )}
+        </div>
+
+        <div className="content">
+         <div className="details">
+          <form onSubmit={handleSubmit}>
+            <label className="hide-file-input">
+                <h3>
+                Choose Avatar
+                </h3>
+            </label>
+                <input
+                  type= "file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="hide-file-input"/>
+
             <label>
-                Username:
+                <h3>
+                Username
+                </h3>
                 <input
                   type= "text"
                   value={username}
                   onChange={(event)=> setUsername(event.target.value)}
                 />
             </label>
-             <label>
-                Change Avatar:
-                <input
-                  type= "file"
-                  accept="image/*"
-                  onChange={handleImageChange}/>
-            </label>
-
-            {avatar && (
-            <img 
-              src={avatar} 
-              alt="avatar"/>
-            )}
+             
+            <div className="actionBtn">
             <button type="submit">Save</button>
+            </div>
         </form>
+       </div>
+     </div>
+    </div>
     )
 }
+
 
 
 export default UserCard
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,14 @@
 
 import { useState } from "react";
 import UserCard from "../components/UserCard"; 
+import Skills from "../components/Skills";
 
 function ProfilePage() {
 
     const [username, setUsername] = useState("coolJmessy");
     const [avatar, setAvatar] = useState("https://example.com/avatar.png");
+    const [skillToTeach, setSkillToTeach] = useState("Japanese");
+    const [skillToLearn, setSkillToLearn] = useState("German");
 
     const handleSave = ( newUsername: string, newAvatar: string) => {
         console.log ("submitted",  newUsername, newAvatar);
@@ -13,16 +16,25 @@ function ProfilePage() {
         setAvatar(newAvatar);
     };
 
+     const handleSkillSave = ( newSkillToTeach: string, newSkillToLearn: string) => {
+        console.log ("submitted Skill",  newSkillToTeach, newSkillToLearn);
+        setSkillToTeach(newSkillToTeach);
+        setSkillToLearn(newSkillToLearn);
+    };
+
     return (
-        <div>
+        <div className="profile-section">
             <h2>Your Profile</h2>
             <UserCard
             username={username}
             avatar={avatar}
             setUsername={setUsername}
             setAvatar={setAvatar}
-            onSave={handleSave}
-            />
+            onSave={handleSave}/>
+            <Skills
+            skillToTeach={skillToTeach}
+            skillToLearn={skillToLearn}
+            onSave={handleSkillSave}/>
         </div>
     );
 }
