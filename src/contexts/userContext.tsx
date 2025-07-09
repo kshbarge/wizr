1aba182bd3bbf41100a48274777028b7073b8c7b
Giubis <116124169+Giubis@users.noreply.github.com>
2025-07-09 14:46:38 +0100

    Create userContext to hold the value of the current user, wrap the whole application with the userContext

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,1 +1,24 @@
-import { useContext } from "react";
+import React, { createContext, useState, type ReactNode } from "react";
+
+interface User {
+  username: string;
+}
+
+type UserContextType = [
+  User | null,
+  React.Dispatch<React.SetStateAction<User | null>>
+];
+
+const UserContext = createContext<UserContextType | null>(null);
+
+export function UserProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default UserContext;
