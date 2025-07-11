c23a29a8c80e43f7e1c1f216dcbe6aad49670c96
kshbarge <kshbarge@posteo.net>
2025-07-11 11:25:05 +0100

    add user video and mic capture

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,39 @@
+import { useState, useEffect, useRef } from "react";
+
 function Video() {
+    const [stream, setStream] = useState()
+
+    const mediaStreamConstraints = {
+    audio: true,
+    video: true
+    }; 
+
+    const myVideo = useRef<HTMLVideoElement | null>(null)
+
+     useEffect(() => {
+        navigator.mediaDevices.getUserMedia( mediaStreamConstraints )
+         .then( (MediaStream) => {
+            setStream(MediaStream)
+            myVideo.current.srcObject = MediaStream
+         })
+         .catch( (error) => {
+            console.log(error);
+         });
+     }, [])
+
     return (
         <>
           <h2>Video Chat</h2>
+          <div>
+            <section>
+                <h3>Local</h3>
+                {stream && <video playsInline ref={myVideo} autoPlay/>}
+            </section>
+            <section>
+                <h3>Remote</h3>
+                <video></video>
+            </section>
+          </div>
         </>
     )
 }
