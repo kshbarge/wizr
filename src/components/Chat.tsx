3e0bf902064f56b21e3d060d96c7326b694a7925
kshbarge <kshbarge@posteo.net>
2025-07-16 10:50:21 +0100

    added user context to chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import UserContext from "../contexts/userContext";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:9628");
@@ -6,8 +7,11 @@ const socket = io("http://localhost:9628");
 function Chat() {
   const [input, setInput] = useState("");
   const [messageData, setMessageData] = useState([
-    { body: "Welcome to the chat window!" },
+    { body: "Welcome to the chat window!", sentBy: "Room" },
   ]);
+
+  const context = useContext(UserContext);
+  const [user] = context;
 
   function handleInputChange(e) {
     setInput(e.target.value);
@@ -15,14 +19,14 @@ function Chat() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    socket.emit("newMessage", { message: input });
+    socket.emit("newMessage", { message: input, sender: user.username });
   }
 
   useEffect(() => {
     socket.on("message", (data) => {
       setMessageData((pastMessages) => [
         ...pastMessages,
-        { body: data.message },
+        { body: data.message, sentBy: data.sender },
       ]);
     });
   }, [socket]);
@@ -32,7 +36,7 @@ function Chat() {
       <h2>User chat</h2>
       <section>
         {messageData.map((msg, index) => (
-          <div key={index}>{msg.body}</div>
+          <div key={index}>{msg.sentBy}: {msg.body}</div>
         ))}
       </section>
       <section>
